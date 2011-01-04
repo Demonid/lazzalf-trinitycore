421b59479f8fd298762a7dc1b88aae4e2233a182
Lazzalf <Lazzalf@localhost>
2011-01-04 22:23:42 +0100

    Fix arena npcs LoS and Typo in summon function by PrinceCreed

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_thorim.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_thorim.cpp
@@ -128,7 +128,7 @@ enum ArenaAdds
     IRON_HONOR_GUARD
 };
 
-const uint32 ARENA_PHASE_ADD[]          = {32876, 32904, 32878, 32877, 32874, 32875};
+const uint32 ARENA_PHASE_ADD[]          = {32876, 32877, 32878, 32904, 32874, 32875, 33110};
 #define SPELL_ARENA_PRIMARY(i)            RAID_MODE(SPELL_ARENA_PRIMARY_N[i],SPELL_ARENA_PRIMARY_H[i])
 const uint32 SPELL_ARENA_PRIMARY_N[]    = {35054, 62326, 62327, 62322, 64151, 42724};
 const uint32 SPELL_ARENA_PRIMARY_H[]    = {35054, 62326, 62445, 62322, 64151, 42724};
@@ -209,11 +209,6 @@ const Position PosCharge[7] =
 {2145.81f, -222.196f, 419.573f, 4.45059f},
 {2123.91f, -222.443f, 419.573f, 4.97419f}
 };
-
-#define POS_X_ARENA  2181.19f
-#define POS_Y_ARENA  -299.12f
-
-#define IN_ARENA(who) (who->GetPositionX() < POS_X_ARENA && who->GetPositionY() > POS_Y_ARENA)
 
 class boss_thorim : public CreatureScript
 {
@@ -349,12 +344,6 @@ class boss_thorim : public CreatureScript
         {
             if (!UpdateVictim())
                 return;
-            
-            if (phase == PHASE_2 && !IN_ARENA(me))
-            {
-                EnterEvadeMode();
-                return;
-            }
                 
             events.Update(diff);
             EncounterTime += diff;
@@ -370,7 +359,7 @@ class boss_thorim : public CreatureScript
                     {
                         case EVENT_STORMHAMMER:
                             if (Unit *pTarget = SelectTarget(SELECT_TARGET_RANDOM, 0, 80, true))
-                                if (pTarget->isAlive() && IN_ARENA(pTarget))
+                                if (pTarget->isAlive() && pTarget->IsWithinLOSInMap(me))
                                     DoCast(pTarget, SPELL_STORMHAMMER);
                             events.ScheduleEvent(EVENT_STORMHAMMER, urand(15000, 20000), 0, PHASE_1);
                             break;
@@ -583,9 +572,7 @@ class mob_arena_phase : public CreatureScript
             id = ArenaAdds(0);
             for (uint8 i = 0; i < 6; ++i)
                 if (me->GetEntry() == ARENA_PHASE_ADD[i])
-                    id = ArenaAdds(i);
-                    
-            IsInArena = IN_ARENA(me);
+                    id = ArenaAdds(i);                    
         }
 
         ArenaAdds id;
@@ -593,41 +580,6 @@ class mob_arena_phase : public CreatureScript
         int32 PrimaryTimer;
         int32 SecondaryTimer;
         int32 ChargeTimer;
-        bool IsInArena;
-        
-        bool isOnSameSide(const Unit *pWho)
-        {
-            return (IsInArena == IN_ARENA(pWho));
-        }
-        
-        void DamageTaken(Unit *attacker, uint32 &damage)
-        {
-            if (!isOnSameSide(attacker))
-                damage = 0;
-        }
-        
-        void EnterEvadeMode()
-        {
-            Map* pMap = me->GetMap();
-            if (pMap->IsDungeon())
-            {
-                Map::PlayerList const &PlayerList = pMap->GetPlayers();
-                if (!PlayerList.isEmpty())
-                {
-                    for (Map::PlayerList::const_iterator i = PlayerList.begin(); i != PlayerList.end(); ++i)
-                    {
-                        if (i->getSource() && i->getSource()->isAlive() && isOnSameSide(i->getSource()))
-                        {
-                            AttackStart(i->getSource());
-                            return;
-                        }
-                    }
-                }
-            }
-
-            me->StopMoving();
-            Reset();
-        }
 
         void Reset()
         {
@@ -640,20 +592,26 @@ class mob_arena_phase : public CreatureScript
         {
             if (id == DARK_RUNE_WARBRINGER)
                 DoCast(me, SPELL_AURA_OF_CELERITY);
+
+            if (!me->IsWithinLOSInMap(me->getVictim()))
+            {
+                if (Unit* pTarget = me->SelectNearestTarget(45))
+                {
+                    me->getThreatManager().modifyThreatPercent(me->getVictim(), -100);
+                    me->AddThreat(pTarget, 100.0f);
+                }
+            }
                 
             DoZoneInCombat();
         }
         
         void UpdateAI(const uint32 uiDiff)
-        {
-            if ((!isOnSameSide(me) || me->getVictim() && !isOnSameSide(me->getVictim())))
-            {
-                EnterEvadeMode();
-                return;
-            }
-                
+        {                
             if (me->HasUnitState(UNIT_STAT_CASTING))
                 return;
+
+            if (!me->IsWithinLOSInMap(me->getVictim()))
+                me->getThreatManager().modifyThreatPercent(me->getVictim(), -100);
                 
             if (PrimaryTimer <= int32(uiDiff))
             {
