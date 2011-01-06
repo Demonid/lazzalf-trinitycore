15e046ed7f67e385d8b6edd216398e25925007a9
Lazzalf <Lazzalf@localhost>
2011-01-06 23:05:15 +0100

    Change an old fix for threat

--- sql/custom/11863_spell_linked_threat.sql
+++ sql/custom/11863_spell_linked_threat.sql
@@ -1,10 +1,7 @@
-DELETE FROM `spell_linked_spell` WHERE `spell_effect` in (57339,-57339);
+--  Tank Class Passive Threat
+DELETE FROM `spell_linked_spell` WHERE `spell_effect` IN (57340);
 INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment`) VALUES
-(21178,57339,0, 'Bear - Tank Class Passive Threat'),
-(-21178,-57339,0, 'Bear - Tank Class Passive Threat'),
-(7376,57339,0, 'Defensive Stance - Tank Class Passive Threat'),
-(-7376,-57339,0, 'Defensive Stance - Tank Class Passive Threat'),
-(48263,57339,0, 'Frost Presence - Tank Class Passive Threat'),
-(-48263,-57339,0, 'Frost Presence - Tank Class Passive Threat'),
-(25780,57339,0, 'Righteous Fury - Tank Class Passive Threat'),
-(-25780,-57339,0, 'Righteous Fury - Tank Class Passive Threat');
+(21178,57340,2, 'Tank Class Passive Threat - Bear Form (Passive2)'),
+(7376,57340,2, 'Tank Class Passive Threat - Defensive Stance Passive'),
+(48263,57340,2, 'Tank Class Passive Threat - Frost Presence'),
+(25780,57340,2, 'Tank Class Passive Threat - Righteous Fury');
