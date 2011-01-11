0da0819dbfd545f9ed4b2e827b5d50b55fa53c7b
Lazzalf <Lazzalf@localhost>
2011-01-11 23:50:58 +0100

    Fix recount Beacon

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6548,6 +6548,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                         {
                             basepoints0 = damage;
                             triggered_spell_id = 53654;
+                            originalCaster = pVictim->GetGUID();
                             target = beaconTarget;
                             break;
                         }
