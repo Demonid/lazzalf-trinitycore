195ab8bbc19fccc34078c19364870d3ac52ecfb2
Lazzalf <Lazzalf@localhost>
2011-01-12 13:51:15 +0100

    Update npc_porting

--- sql/custom/porting/extra_porting.sql
+++ sql/custom/porting/extra_porting.sql
@@ -1,11 +1,14 @@
 /*Table structure for table `porting` */
+DROP TABLE IF EXISTS `porting`;
 CREATE TABLE `porting` (
   `guid` int(32) unsigned NOT NULL,
   `active` int(8) NOT NULL DEFAULT '0',
   `fase` int(8) NOT NULL DEFAULT '0',
   `level` int(8) NOT NULL DEFAULT '0',
   `gold` int(32) NOT NULL DEFAULT '0',
-  `id_equip` int(32) NOT NULL DEFAULT '0',
+  `id_equip_1` int(32) NOT NULL DEFAULT '0',
+  `id_equip_2` int(32) NOT NULL DEFAULT '0',
+  `id_equip_3` int(32) NOT NULL DEFAULT '0',
   `repu_1` int(32) NOT NULL DEFAULT '0',
   `level_repu_1` int(32) NOT NULL DEFAULT '0',
   `repu_2` int(32) NOT NULL DEFAULT '0',
