d38b2d2ce92afc08e0a9ad943400c4a23fd8f431
MarcoNike <lightshard@hotmail.it>
2011-01-21 17:35:03 +0100

    - Ancora sul Lunar Festival

---
+++ src/server/game/Custom/AntiCheat.h
@@ -0,0 +1,133 @@
+#ifndef ANTI_CHEAT_H
+#define ANTI_CHEAT_H
+
+/**
+ *
+ * @File : AntiCheat.h
+ *
+ * @Authors : Lazzalf based on AC2 and Manuel AntiCheat
+ *
+ **/
+
+#include "Common.h"
+#include "WorldSession.h"
+
+enum eCheat
+{
+	CHEAT_GRAVITY = 0,
+	CHEAT_MULTIJUMP,
+	CHEAT_SPEED,
+	CHEAT_TELEPORT,
+	CHEAT_MOUNTAIN,
+	CHEAT_FLY,
+	CHEAT_WATERWALK,
+	CHEAT_TELETOPLANE,
+    MAX_CHEAT
+};
+
+enum ePuniType
+{
+    PUNI_NONE = 0,
+    PUNI_BLOCK,
+	PUNI_KILL,
+	PUNI_KICK,
+	PUNI_BAN_CHAR,
+    PUNI_BAN_ACC,
+    PUNI_BAN_IP,
+    MAX_PUNI
+};
+
+class AntiCheat
+{
+    private:
+        Player* plMover;
+
+		bool ac_block;        
+		int32 ac_delta;
+
+        MovementInfo lastpMovementInfo;
+        uint32 uiLastOpcode;
+
+        bool cheat_find;
+
+        bool map_count;
+        bool map_block;
+        bool map_puni;
+
+		// Delthas
+		uint64 cServerTime;		
+		uint32 cServerTimeDelta;
+        uint32 difftime_log_file;
+        uint32 difftime_log_db;
+        uint32 uiDiffTime_packets;
+
+		// Variables
+		float fSpeedRate;	
+        uint32 uSpeedRate;
+		float delta_z;
+		bool fly_auras;
+		bool swim_in_water;
+		float tg_z;
+		float JumpHeight;
+
+        float fClientSpeedRate;
+        float fDistance2d;
+
+        uint32 uClientSpeedRate;
+        uint32 uDistance2D;
+		
+		void CalcDeltas(MovementInfo& pNewPacket, MovementInfo& pOldPacket);
+		void CalcVariables(MovementInfo& pOldPacket, MovementInfo& pNewPacket, Unit* mover);
+        bool CanFly(MovementInfo& pMovementInfo);
+		
+		bool CheckAntiGravity(MovementInfo& /*pMovementInfo*/);
+		bool CheckAntiMultiJump(MovementInfo& pNewPacket, uint32 uiOpcode);
+        bool CheckAntiSpeed(MovementInfo& pOldPacket, MovementInfo& pNewPacket, uint32 uiOpcode);
+        bool CheckAntiTele(MovementInfo& pNewPacket, uint32 uiOpcode);
+		bool CheckAntiMountain(MovementInfo& /*pMovementInfo*/);
+		bool CheckAntiFly(MovementInfo& pOldPacket, MovementInfo& pNewPacket);
+		bool CheckAntiWaterwalk(MovementInfo& pOldPacket, MovementInfo& pNewPacket);
+		bool CheckAntiTeleToPlane(MovementInfo& pOldPacket, MovementInfo& pNewPacket);
+        void LogCheat(eCheat /*m_cheat*/, MovementInfo& /*pMovementInfo*/);
+        bool AntiCheatPunisher(MovementInfo& /*pMovementInfo*/);
+        inline bool ControllPunisher();
+	
+    public:
+        AntiCheat(Player* new_plMover);
+		bool DoAntiCheatCheck(uint16 /*opcode*/, MovementInfo& /*pMovementInfo*/, Unit* /*mover*/);
+
+    public:
+        uint32 number_cheat_find;
+        int32 ac_goactivate;
+
+        uint32 m_CheatList[MAX_CHEAT];
+        uint32 m_CheatList_reset_diff;        
+	
+        time_t m_anti_LastServerTime;           // last movement server time
+        time_t m_anti_DeltaServerTime;          // server side session time
+        time_t m_logfile_time;
+        time_t m_logdb_time;
+
+        float m_anti_Last_VSpeed;               // vertical speed, default max jump height
+
+        uint32 m_anti_TeleToPlane_Count;        // Teleport To Plane alarm counter
+
+        uint64 m_anti_AlarmCount;               // alarm counter
+
+        uint16 m_anti_JumpCount;                // Jump already began, anti air jump check
+        float m_anti_JumpBaseZ;                 // Z coord before jump
+		
+		void SetBlock(bool /*block*/);
+		bool GetBlock();
+		bool GetAndUpdateDelta(int32 /*diff*/);
+		void SetDelta(int32 /*delta*/);
+        int32 GetDelta();
+        void SetSleep(int32 delta, bool forced = true);
+        void ResetCheatList(uint32 /*diff*/);
+        void SaveLastPacket(MovementInfo& pMovementInfo) { lastpMovementInfo = pMovementInfo; }
+        MovementInfo& GetLastPacket() { return lastpMovementInfo; }
+        void SetLastOpcode(uint32 uiOpcode) { uiLastOpcode = uiOpcode; }
+        uint32 GetLastOpcode() { return uiLastOpcode; }
+};
+
+#endif
