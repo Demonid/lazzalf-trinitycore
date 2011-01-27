1f440be18a2898e4096dd7e13bc065495129efe3
Lazzalf <Lazzalf@localhost>
2011-01-27 17:48:49 +0100

    [Hunter] Some improvements in exotic pet scaling and following distance. By PrinceCreed

---
+++ sql/custom/23626_mimiron.sql
@@ -0,0 +1,32 @@
+-- Mimiron
+UPDATE `creature_template` SET `mechanic_immune_mask` = 650854235, `ScriptName` = 'boss_mimiron' WHERE `entry` = 33350;
+-- Leviathan MKII
+UPDATE `creature_template` SET `vehicleid` = 370, `mechanic_immune_mask` = 650854235, `ScriptName` = 'boss_leviathan_mk' WHERE `entry` = 33432;
+UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83, `mechanic_immune_mask` = 650854235, `flags_extra` = 1 WHERE `entry` = 34106;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 650854235, `ScriptName` = 'boss_leviathan_mk_turret' WHERE `entry` = 34071;
+DELETE FROM vehicle_accessory WHERE entry = 33432;
+INSERT INTO vehicle_accessory VALUE (33432, 34071, 3, 1, 'Leviathan Mk II turret');
+UPDATE creature_template SET ScriptName = 'npc_proximity_mine' WHERE entry = 34362;
+DELETE FROM `creature_model_info` WHERE `modelid`=28831;
+INSERT INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES
+(28831, 0.5, 7, 2, 0);
+-- VX-001
+UPDATE `creature_template` SET `mechanic_immune_mask` = 650854235, `flags_extra` = 1, `vehicleid` = 371, `ScriptName` = 'boss_vx_001' WHERE `entry` = 33651;
+UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83, `mechanic_immune_mask` = 650854235, `flags_extra` = 1 WHERE `entry` = 34108;
+UPDATE `creature_template` SET `faction_A` = 35, `faction_H` = 35 WHERE `entry` = 34050;
+UPDATE `creature_template` SET `unit_flags` = 33686020, `flags_extra` = 2 WHERE `entry` = 34211;
+UPDATE `creature_template` SET `ScriptName` = 'npc_rocket_strike' WHERE `entry` = 34047;
+-- Aerial Command Unit
+UPDATE `creature_template` SET `mechanic_immune_mask` = 650854235, `flags_extra` = 1, `ScriptName` = 'boss_aerial_unit' WHERE `entry` = 33670;
+UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83, `mechanic_immune_mask` = 650854235, `flags_extra` = 1 WHERE `entry` = 34109;
+UPDATE `creature_template` SET `ScriptName` = 'npc_magnetic_core' WHERE `entry` = 34068;
+UPDATE `creature_template` SET `ScriptName` = 'npc_assault_bot' WHERE `entry` = 34057;
+UPDATE `creature_template` SET `difficulty_entry_1` = 34148, `ScriptName` = 'npc_emergency_bot' WHERE `entry` = 34147;
+-- HardMode
+UPDATE `gameobject_template` SET `flags` = 32, `ScriptName` = 'go_not_push_button' WHERE `entry` = 194739;
+UPDATE `creature_template` SET `difficulty_entry_1` = 34361, `ScriptName` = 'npc_frost_bomb' WHERE `entry` = 34149;
+UPDATE `creature_template` SET `speed_walk` = 0.15, `speed_run` = 0.15, `ScriptName` = 'npc_mimiron_flame_trigger' WHERE `entry` = 34363;
+UPDATE `creature_template` SET `ScriptName` = 'npc_mimiron_flame_spread' WHERE `entry` = 34121;
+-- CleanUp
+DELETE FROM creature WHERE id IN (34071, 33856);
+UPDATE `creature_template` SET `flags_extra` = 2 WHERE `entry` = 34143;
---
+++ src/server/game/Custom/AntiCheat.h
@@ -0,0 +1,113 @@
+#ifndef ANTI_CHEAT_H
+#define ANTI_CHEAT_H
+
+/**
+ *
+ * @File : AntiCheat.h
+ *
+ * @Authors : Lazzalf based on AC2 and Manuel AntiCheat
+ *
+ **/
+
+#include "Common.h"
+#include "WorldSession.h"
+
+enum eCheat
+{
+	CHEAT_MULTIJUMP = 0,
+	CHEAT_SPEED,
+	CHEAT_TELEPORT,
+	CHEAT_FLY,
+	CHEAT_WATERWALK,
+	CHEAT_TELETOPLANE,
+    MAX_CHEAT
+};
+
+enum ePuniType
+{
+    PUNI_NONE = 0,
+    PUNI_BLOCK,
+	PUNI_KILL,
+	PUNI_KICK,
+	PUNI_BAN_CHAR,
+    PUNI_BAN_ACC,
+    PUNI_BAN_IP,
+    MAX_PUNI
+};
+
+class AntiCheat
+{
+    private:
+        Player* plMover;
+
+        bool activateACCheck;
+        uint32 alarmACCheckTimer;
+        uint32 disableACCheckTimer;
+
+        MovementInfo lastpMovementInfo;
+        uint32 uiLastOpcode;
+
+        bool cheat_find;
+
+        bool map_count;
+        bool map_block;
+        bool map_puni;
+
+		// Delthas
+		uint64 cServerTime;		
+		uint32 cServerTimeDelta;
+        uint32 difftime_log_file;
+        uint32 difftime_log_db;
+        uint32 uiDiffTime_packets;
+
+		// Variables
+        uint32 uSpeedRate;         
+        uint32 uClientSpeedRate;
+        uint32 uDistance2D;
+
+		bool fly_auras;
+
+        uint32 number_cheat_find;
+        int32 ac_goactivate;
+
+        uint32 m_CheatList[MAX_CHEAT];
+        uint32 m_CheatList_reset_diff;        
+	
+        time_t m_anti_LastServerTime;           // last movement server time
+        time_t m_anti_DeltaServerTime;          // server side session time
+        time_t m_logfile_time;
+        time_t m_logdb_time;
+
+        uint32 m_anti_TeleToPlane_Count;        // Teleport To Plane alarm counter
+		
+		void CalcDeltas(MovementInfo& pNewPacket, MovementInfo& pOldPacket);
+		void CalcVariables(MovementInfo& pOldPacket, MovementInfo& pNewPacket, Unit* mover);
+        bool CanFly(MovementInfo& pMovementInfo);
+		
+		bool CheckAntiMultiJump(MovementInfo& pNewPacket, uint32 uiOpcode);
+        bool CheckAntiSpeed(MovementInfo& pOldPacket, MovementInfo& pNewPacket, uint32 uiOpcode);
+        bool CheckAntiTele(MovementInfo& pNewPacket, uint32 uiOpcode);
+		bool CheckAntiFly(MovementInfo& pOldPacket, MovementInfo& pNewPacket);
+		bool CheckAntiWaterwalk(MovementInfo& pOldPacket, MovementInfo& pNewPacket);
+		bool CheckAntiTeleToPlane(MovementInfo& pOldPacket, MovementInfo& pNewPacket);
+        void LogCheat(eCheat /*m_cheat*/, MovementInfo& /*pMovementInfo*/);
+        bool AntiCheatPunisher(MovementInfo& /*pMovementInfo*/);
+        inline bool ControllPunisher();
+	
+    public:
+        AntiCheat(Player* new_plMover);
+		bool DoAntiCheatCheck(uint16 /*opcode*/, MovementInfo& /*pMovementInfo*/, Unit* /*mover*/);
+	
+        void UpdateDiffAntiCheat(uint32 diff);
+        void UpdateAntiCheat();
+		void SetAlarm(uint32 delta);
+        int32 GetDelta();
+        void SetSleep(uint32 delta);
+        void ResetCheatList(uint32 /*diff*/);
+        void SaveLastPacket(MovementInfo& pMovementInfo) { lastpMovementInfo = pMovementInfo; }
+        MovementInfo& GetLastPacket() { return lastpMovementInfo; }
+        void SetLastOpcode(uint32 uiOpcode) { uiLastOpcode = uiOpcode; }
+        uint32 GetLastOpcode() { return uiLastOpcode; }
+};
+
+#endif
