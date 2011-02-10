0851271ebd440218e43ac10a3cbf8e531254d076
Lazzalf <Lazzalf@localhost>
2011-02-10 08:35:24 +0100

    Fix compile

--- src/server/scripts/Outland/shadowmoon_valley.cpp
+++ src/server/scripts/Outland/shadowmoon_valley.cpp
@@ -1860,7 +1860,7 @@ public:
 
                      Unit* Owner = totemOspirits->GetOwner();
                      if (Owner && Owner->GetTypeId() == TYPEID_PLAYER)
-                         Owner->ToPlayer->KilledMonsterCredit(credit, 0);
+                         Owner->ToPlayer()->KilledMonsterCredit(credit, 0);
                          // DoCast(Owner, credit); -- not working!
                          //CAST_PLR(Owner)->KilledMonsterCredit(credit, Summoned->GetGUID());
                      DoCast(totemOspirits, SPELL_SOUL_CAPTURED);
