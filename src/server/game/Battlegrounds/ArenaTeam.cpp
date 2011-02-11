e3b460db91f7ef7748bba907bf1462a4f912d24c
Lazzalf <Lazzalf@localhost>
2011-02-11 15:48:09 +0100

    [Arena] Another change

--- src/server/game/Battlegrounds/ArenaTeam.cpp
+++ src/server/game/Battlegrounds/ArenaTeam.cpp
@@ -787,9 +787,9 @@ void ArenaTeam::MemberWon(Player * plr, uint32 againstMatchmakerRating, int32 te
                 if (enemy_rating < 1000)
                     enemy_rating = 1000;
                 float chance = GetChanceAgainst(itr->personal_rating, enemy_rating);
-                float K = (itr->personal_rating < 1000) ? 48.0f : 32.0f;
+                //float K = (itr->personal_rating < 1000) ? 48.0f : 32.0f;
                 // calculate the rating modification (ELO system with k=32 or k=48 if rating<1000)
-                mod = (int32)floor(K* (1.0f - chance));
+                mod = (int32)floor(teamratingchange* (2.0f - chance));
             }
             else
             {
