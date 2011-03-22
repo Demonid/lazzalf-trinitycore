56702dc0e2223118840a48f3689a301f1d1293a9
Lazzalf <Lazzalf@localhost>
2011-03-22 08:35:39 +0100

    Fix compile

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -433,7 +433,7 @@ void AddSC_boss_freya();
 void AddSC_boss_thorim();
 void AddSC_boss_mimiron();
 void AddSC_boss_general_vezax();
-void AddSC_boss_yogg_saron();
+void AddSC_boss_yoggsaron();
 void AddSC_ulduar_teleporter();
 void AddSC_instance_ulduar();
 void AddSC_boss_keleseth();              //Utgarde Keep
@@ -1140,7 +1140,7 @@ void AddNorthrendScripts()
     AddSC_boss_thorim();
  	AddSC_boss_mimiron();
  	AddSC_boss_general_vezax();
-    AddSC_boss_yogg_saron();
+    AddSC_boss_yoggsaron();
     AddSC_ulduar_teleporter();
     AddSC_instance_ulduar();
     AddSC_boss_keleseth();              //Utgarde Keep
