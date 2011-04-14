7a2785a7122fe20437aa6b223f40d52177b8eb95
Lazzalf <Lazzalf@localhost>
2011-04-14 15:27:01 +0200

    [Mechanic] Fixato possibile uso di pi? pozioni assieme.

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1693,6 +1693,7 @@ class Player : public Unit, public GridObject<Player>
         void _LoadSpellCooldowns(PreparedQueryResult result);
         void _SaveSpellCooldowns(SQLTransaction& trans);
         void SetLastPotionId(uint32 item_id) { m_lastPotionId = item_id; }
+        uint32 GetLastPotionId() { return m_lastPotionId; } 
         void UpdatePotionCooldown(Spell* spell = NULL);
 
         // global cooldown
