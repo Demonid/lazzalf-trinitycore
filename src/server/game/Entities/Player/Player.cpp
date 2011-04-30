0e2f77226643b3f222586d5157cfd8ba6f903c2b
Lazzalf <Lazzalf@localhost>
2011-04-30 12:16:31 +0200

    Fixed a bug with disabling PvP state at hostile zones. Patch by tobmaps

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19323,6 +19323,8 @@ void Player::UpdatePvPFlag(time_t currTime)
         return;
     if (pvpInfo.endTimer == 0 || currTime < (pvpInfo.endTimer + 300))
         return;
+    if (pvpInfo.inHostileArea)
+        return;
 
     UpdatePvP(false);
 }
