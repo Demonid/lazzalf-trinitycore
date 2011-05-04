dc9d509bf4891a2e510292a1e89fbaaa5007d63f
Lazzalf <Lazzalf@localhost>
2011-05-04 07:56:28 +0200

    Backout eseguito sul changeset: eba0f54d99dd

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -77,6 +77,7 @@ enum WorldTimers
     WUPDATE_MAILBOXQUEUE,
     WUPDATE_DELETECHARS,
     WUPDATE_PINGDB,
+    WUPDATE_WARDEN, // Because I don't want to create yet another thread
     WUPDATE_COUNT
 };
 
@@ -365,7 +366,8 @@ enum WorldIntConfigs
     CONFIG_AC_RESET_CHEATLIST_DELTA,
     CONFIG_AC_RESET_CHEATLIST_DELTA_FOUND,
     CONFIG_AC_PUNI_TYPE,
-    CONFIG_AC_PUNI_LEVEL_MAX,  
+    CONFIG_AC_PUNI_LEVEL_MAX,
+    CONFIG_UINT32_WARDEN_BAN_TIME,
     INT_CONFIG_VALUE_COUNT
 };
 
@@ -756,6 +758,7 @@ class World
         void KickAll();
         void KickAllLess(AccountTypes sec);
         BanReturn BanAccount(BanMode mode, std::string nameOrIP, std::string duration, std::string reason, std::string author);
+        BanReturn BanAccount(WorldSession *session, uint32 duration_secs, std::string reason, std::string author);
         bool RemoveBanAccount(BanMode mode, std::string nameOrIP);
         BanReturn BanCharacter(std::string name, std::string duration, std::string reason, std::string author);
         bool RemoveBanCharacter(std::string name);
