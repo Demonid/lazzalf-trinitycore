712ffa8da69038f8506a9a2148ad2b07d1d69593
Lazzalf <Lazzalf@localhost>
2011-05-12 11:27:13 +0200

    Typo

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -9202,7 +9202,7 @@ void Player::SendInitWorldStates(uint32 zoneid, uint32 areaid)
             break;
         //case 4100:  // The Culling of Stratholme
         //    NumberOfFields = 13;
-            break;
+        //    break;
          default:
             NumberOfFields = 12;
             break;
