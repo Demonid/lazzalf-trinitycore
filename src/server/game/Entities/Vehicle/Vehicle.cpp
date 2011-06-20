2b04784ce2160a8a809d450523ccdf990b76d8c2
Lazzalf <Lazzalf@localhost>
2011-06-20 15:18:09 +0200

    Vehicle: Disattivato il bonus HP dei veicoli (a volte li distrugge)

--- src/server/game/Entities/Vehicle/Vehicle.cpp
+++ src/server/game/Entities/Vehicle/Vehicle.cpp
@@ -400,6 +400,7 @@ bool Vehicle::AddPassenger(Unit* unit, int8 seatId)
             ASSERT(false);
 
         // hack: should be done by aura system
+        /*
         if (VehicleScalingInfo const* scalingInfo = sObjectMgr->GetVehicleScalingInfo(m_vehicleInfo->m_ID))
         {
             Player* player = unit->ToPlayer();
@@ -413,6 +414,7 @@ bool Vehicle::AddPassenger(Unit* unit, int8 seatId)
             me->SetMaxHealth(me->GetMaxHealth() + m_bonusHP);
             me->SetHealth(uint32((me->GetHealth() + m_bonusHP) * currentHealthPct));
         }
+        */
     }
 
     if (me->IsInWorld())
