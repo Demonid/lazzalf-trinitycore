1c8f96021a3d471c629da8382abfffc5f8c20258
Lazzalf <Lazzalf@localhost>
2011-08-26 18:37:10 +0200

    Core/Maps: Crash fix. Prevents access violation caused by NULL obj pointer. By LihO.

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -2037,6 +2037,12 @@ void Map::RemoveAllObjectsInRemoveList()
         std::set<WorldObject*>::iterator itr = i_objectsToRemove.begin();
         WorldObject* obj = *itr;
 
+        if (!obj)
+        {
+            i_objectsToRemove.erase(itr);
+            continue;
+        }
+
         switch(obj->GetTypeId())
         {
             case TYPEID_CORPSE:
