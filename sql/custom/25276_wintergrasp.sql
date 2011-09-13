e0fada73a1706e75b2c1c6e6805010734a9ce9c9
Lazzalf <Lazzalf@localhost>
2011-09-13 21:48:29 +0200

    Backout eseguito sul changeset: be926c79c965

---
+++ sql/custom/25276_wintergrasp.sql
@@ -0,0 +1,30 @@
+﻿/* Teleport WG SPELLs*/
+DELETE FROM `spell_target_position` WHERE id IN ('59096', '58632', '58633');
+INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
+('59096', '571', '5325.06', '2843.36', '409.285', '3.20278'),
+('58632', '571', '5097.79', '2180.29', '365.61', '2.41'),
+('58633', '571', '5026.80', '3676.69', '362.58', '3.94');
+
+/* Portal Dalaran->WG */
+DELETE FROM `gameobject` WHERE `id`=193772;
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+('', 193772, 571, 1, 64, 5924.11, 573.387, 661.087, 4.43208, 0, 0, 0.798953, -0.601393, 300, 0, 1),
+('', 193772, 571, 1, 65535, 5686.57, 772.921, 647.754, 5.62225, 0, 0, 0.324484, -0.945891, 600, 0, 1),
+('', 193772, 571, 1, 65535, 5930.82, 548.961, 640.632, 1.88506, 0, 0, 0.809047, 0.587744, 300, 0, 1);
+
+/* Wintergrasp Battle-Mage */
+DELETE FROM `creature` WHERE `id`=32170;
+INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
+('', 32170, 571, 1, 65535, 27801, 0, 5925.34, 573.71, 661.087, 4.06662, 300, 0, 0, 504000, 440700, 0, 0, 0, 0, 0),
+('', 32170, 571, 1, 64, 0, 918, 5932.96, 549.906, 641.595, 2.09189, 300, 0, 0, 504000, 440700, 0, 0, 0, 0, 0);
+UPDATE `creature_template` SET `ScriptName`='npc_wg_ally_battle_mage' WHERE `entry`=32169;
+UPDATE `creature_template` SET `ScriptName`='npc_wg_horde_battle_mage' WHERE `entry`=32170;
+
+/*Spirit healer FIX */
+UPDATE creature_template SET npcflag=npcflag|32768 WHERE entry IN (31841,31842);
+
+/* Protect players from catching by Druid Cyclone at graveyard which removes immunity after disappear */
+DELETE FROM `spell_linked_spell` WHERE spell_trigger=58729;
+INSERT INTO `spell_linked_spell` VALUES (58729, -33786, 2, 'Spiritual Immunity: Protect From Cyclone (now Immune always)');
+
+UPDATE `gameobject_template` SET `type` = 6, `faction` = 0, `data2` = 10, `data3` = 54643, `ScriptName` = 'go_wg_veh_teleporter' WHERE `entry` = 192951;
---
+++ src/server/scripts/Northrend/dragonblight.cpp
@@ -0,0 +1,228 @@
+/*
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* ScriptData
+SDName: Dragonblight
+SD%Complete: 100
+SDComment:
+SDCategory: Dragonblight
+EndScriptData */
+
+/* ContentData
+npc_alexstrasza_wr_gate
+EndContentData */
+
+#include "ScriptPCH.h"
+#include "ScriptedEscortAI.h"
+
+enum eEnums
+{
+    QUEST_RETURN_TO_AG_A    = 12499,
+    QUEST_RETURN_TO_AG_H    = 12500,
+    MOVIE_ID_GATES          = 14
+};
+
+#define GOSSIP_ITEM_WHAT_HAPPENED   "Alexstrasza, can you show me what happened here?"
+
+class npc_alexstrasza_wr_gate : public CreatureScript
+{
+public:
+    npc_alexstrasza_wr_gate() : CreatureScript("npc_alexstrasza_wr_gate") { }
+
+    bool OnGossipHello(Player* player, Creature* creature)
+    {
+        if (creature->isQuestGiver())
+            player->PrepareQuestMenu(creature->GetGUID());
+
+        if (player->GetQuestRewardStatus(QUEST_RETURN_TO_AG_A) || player->GetQuestRewardStatus(QUEST_RETURN_TO_AG_H))
+            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_WHAT_HAPPENED, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+
+        player->SEND_GOSSIP_MENU(player->GetGossipTextId(creature), creature->GetGUID());
+        return true;
+    }
+
+    bool OnGossipSelect(Player* player, Creature* /*creature*/, uint32 /*uiSender*/, uint32 uiAction)
+    {
+        player->PlayerTalkClass->ClearMenus();
+        if (uiAction == GOSSIP_ACTION_INFO_DEF+1)
+        {
+            player->CLOSE_GOSSIP_MENU();
+            player->SendMovieStart(MOVIE_ID_GATES);
+        }
+
+        return true;
+    }
+};
+
+/* Emblazoned Battle Horn */
+
+#define NPC_GROMTHAR 27002
+
+const Position gromthar_position = {2720.921f, 427.441f, 67.002f, 5.996f};
+
+class item_emblazoned_battle_horn : public ItemScript
+{
+public:
+    item_emblazoned_battle_horn() : ItemScript("item_emblazoned_battle_horn") { }
+
+    bool OnUse(Player* pPlayer, Item* pItem, SpellCastTargets const& /*targets*/)
+    {
+        if (!pPlayer)
+            return true;
+
+        if (Creature* creature = pPlayer->FindNearestCreature(NPC_GROMTHAR, 100, true))
+            if (creature->isAlive())
+                return false;
+ 
+        pPlayer->SummonCreature(NPC_GROMTHAR, gromthar_position, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 180000);
+        return true;
+    }
+};
+
+/* Torturer LeCraft */
+
+#define SPELL_EXECUTOR_BRANDING_IRON 48603
+#define NPC_TORTURER_CREDIT 27394
+
+class npc_torturer_lecraft : public CreatureScript
+{
+public:
+    npc_torturer_lecraft() : CreatureScript("npc_torturer_lecraft") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_torturer_lecraftAI(creature);
+    }
+
+    struct npc_torturer_lecraftAI : public ScriptedAI
+    {
+        npc_torturer_lecraftAI(Creature *c) : ScriptedAI(c)
+        {
+            brandedTimes = 0;
+        }
+
+        uint8 brandedTimes;
+
+        void Reset()
+        {
+            brandedTimes = 0;
+        }
+
+        void EnterCombat(Unit* /*who*/)
+        {
+            brandedTimes = 0;
+            me->MonsterSay("Come to play?", LANG_UNIVERSAL, 0);
+        }
+
+        void SpellHit(Unit* caster, const SpellInfo *spell)
+        {
+            if (Player* player = caster->ToPlayer())
+            {
+                if (spell->Id == SPELL_EXECUTOR_BRANDING_IRON)
+                {
+                    brandedTimes++;
+
+                    switch (brandedTimes)
+                    {
+                        case 1:
+                            me->MonsterWhisper("Ow! I'll tell you NOTHING, filthy $R!", player->GetGUID());
+                            break;
+                        case 2:
+                            me->MonsterWhisper("Wait... WAIT! What is it that you want to know? I know you're the $C named $N.", player->GetGUID());
+                            break;
+                        case 3:
+                            me->MonsterWhisper("OW...NO! We know that you've been stealing our armor and weapons and horses!", player->GetGUID());
+                            break;
+                        case 4:
+                            me->MonsterWhisper("We know... that you don't... know why we're immune... to your so-called blight. Grand Admiral Westwind somehow gave the high abbot that prayer. I beg you... no more... please?", player->GetGUID());
+                            break;
+                        case 5:
+                            me->MonsterWhisper("AHHHHHHHHH! Please... we know... that you... have a spy... disguised as... one of us! There... that's all that I know. Please... mercy... STOP!", player->GetGUID());
+                            player->KilledMonsterCredit(NPC_TORTURER_CREDIT, 0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        void UpdateAI(const uint32 /*diff*/)
+        {
+            if (!UpdateVictim())
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+
+};
+
+#define NEXUS_DRAKE_HATCHLING 26127
+
+class npc_dummy_raelorasz : public CreatureScript
+{
+public:
+    npc_dummy_raelorasz() : CreatureScript("npc_dummy_raelorasz") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_dummy_raeloraszAI(creature);
+    }
+
+    struct npc_dummy_raeloraszAI : public ScriptedAI
+    {
+        npc_dummy_raeloraszAI(Creature *c) : ScriptedAI(c)
+        {
+            uiFindTimer = 5000;
+        }
+
+        uint32 uiFindTimer;
+
+        void UpdateAI(const uint32 uiDiff)
+        {
+            if (uiFindTimer <= uiDiff)
+            {
+                std::list<Creature*> list;
+                me->GetCreatureListWithEntryInGrid(list, NEXUS_DRAKE_HATCHLING, 30.0f);
+                if (!list.empty())
+                    for (std::list<Creature*>::iterator itr = list.begin(); itr != list.end(); ++itr)
+                    {
+                        if (Unit* owner = (*itr)->GetOwner())
+                            if (owner->ToPlayer())
+                            {
+                                owner->ToPlayer()->CompleteQuest(11940);
+                                (*itr)->DespawnOrUnsummon();
+                            }
+                    }
+                uiFindTimer = 5000;
+            }
+            else
+                uiFindTimer -= uiDiff;
+        }
+    };
+
+};
+
+void AddSC_dragonblight()
+{
+    new npc_alexstrasza_wr_gate;
+    new item_emblazoned_battle_horn();
+    new npc_torturer_lecraft();
+    new npc_dummy_raelorasz();
+}
