5ad90a6856ac1b5d9915e515c981c06e186d3feb
a <a@a-PC.Digicom-ADSL>
2011-09-23 13:42:36 +0200

    retry fix boss_falric

---
+++ sql/custom/27095_spell_trollgore_consume.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (49380,59803);
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`)
+VALUES
+(49380,'spell_trollgore_consume'),
+(59803,'spell_trollgore_consume');
