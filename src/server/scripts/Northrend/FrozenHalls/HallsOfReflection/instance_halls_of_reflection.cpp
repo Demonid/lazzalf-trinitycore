b6d80f0fe124fb49736ed70c86ef34a8f9e14b47
brogi federico <brogi.federico@gmail.com>
2011-09-28 22:29:53 +0200

    Try fix reset lich run

--- src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/instance_halls_of_reflection.cpp
+++ src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/instance_halls_of_reflection.cpp
@@ -264,10 +264,9 @@ public:
             // if main event is in progress and players have wiped then reset instance
             if ((GetData(TYPE_MARWYN) == IN_PROGRESS ||
                  GetData(TYPE_MARWYN) == SPECIAL ||
-                 //GetData(TYPE_MARWYN) == DONE || //Boss reser if the party kill it
                  GetData(TYPE_FALRIC) == IN_PROGRESS ||
-                 GetData(TYPE_FALRIC) == SPECIAL ||            
-                 GetData(TYPE_FALRIC) == DONE) //Boss reser if the party kill it
+                 GetData(TYPE_FALRIC) == SPECIAL ||
+                 GetData(TYPE_LICH_KING) == IN_PROGRESS)
                  && CheckWipe())
             {
                 OpenDoor(m_uiExitGateGUID);
@@ -289,7 +288,17 @@ public:
                         else if (Marwyn->GetAI())
                             Marwyn->GetAI()->SetData(0,0);
                     }
+                if (Creature* Lider = instance->GetCreature(m_uiLiderGUID))
+                { 
+                    if (Lider->isDead())
+                        Lider->Respawn();
+                    else if (Lider->GetAI())
+                        Lider->GetAI()->SetData(0,0);
                 }
+                      
+                }
+
+
             }
         }
 
