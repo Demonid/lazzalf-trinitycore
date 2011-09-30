c33add1e6bd8ec20f8b68c530834f93d4399f459
brogi federico <brogi.federico@gmail.com>
2011-09-30 02:32:11 +0200

    Fix reset lich

--- src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/halls_of_reflection.cpp
+++ src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/halls_of_reflection.cpp
@@ -766,7 +766,22 @@ public:
             }
             if (m_pInstance->GetData(TYPE_LICH_KING) == DONE)
                 me->SetVisible(false);
-           
+            if (m_pInstance->GetData(TYPE_LICH_KING) == FAIL)
+                
+                m_uiFrostGeneralGUID = m_pInstance->GetData64(NPC_FROST_GENERAL);
+            if (Creature* FrostGeneral =m_pInstance->instance->GetCreature(m_uiFrostGeneralGUID))
+                   
+                            if (FrostGeneral->isDead())
+                            {
+                                if (Creature*LichKing = m_pInstance->instance->GetCreature(m_uiLichKingGUID))
+                                {
+            
+                                    LichKing->DespawnOrUnsummon();
+                                }
+        
+
+                            FrostGeneral->Respawn();
+                            }
         }
          
 
@@ -791,20 +806,7 @@ public:
                 return;
             m_pInstance->SetData(TYPE_LICH_KING, FAIL);
              
-            m_uiFrostGeneralGUID = m_pInstance->GetData64(NPC_FROST_GENERAL);
-            if (Creature* FrostGeneral =m_pInstance->instance->GetCreature(m_uiFrostGeneralGUID))
-                   
-                            if (FrostGeneral->isDead())
-                            {
-                                if (Creature*LichKing = m_pInstance->instance->GetCreature(m_uiLichKingGUID))
-                                {
-            
-                                    LichKing->DespawnOrUnsummon();
-                                }
-        
-
-                            FrostGeneral->Respawn();
-                            }
+           
             
         }
 
