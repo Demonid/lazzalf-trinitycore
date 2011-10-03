57b8545d4eb00defbc9b502d46d0a32b81f5a241
brogi federico <brogi.federico@gmail.com>
2011-10-03 02:01:44 +0200

    increse the lich king speed MOVE_WALK, 1.8f

---
+++ sql/custom/27286_spell_script_names.sql
@@ -0,0 +1,4 @@
+DELETE FROM spell_script_names WHERE spell_id = 63276;
+INSERT INTO spell_script_names VALUE (63276,'spell_general_vezax_mark_of_the_faceless_aura');
+DELETE FROM `spell_script_names` WHERE `spell_id`=63278;
+INSERT INTO `spell_script_names` VALUE (63278, 'spell_general_vezax_mark_of_the_faceless_drain');
