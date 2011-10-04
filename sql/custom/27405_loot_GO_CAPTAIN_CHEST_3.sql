06a7c800a2033b7e7d49205469b050f769cffd86
Lazzalf <Lazzalf@localhost>
2011-10-04 23:14:16 +0200

    Codice di Debug per Thaddius

---
+++ sql/custom/27405_loot_GO_CAPTAIN_CHEST_3.sql
@@ -0,0 +1,30 @@
+/*
+SQLyog Community v9.20 
+MySQL - 5.1.49-1ubuntu8.1 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `gameobject_loot_template` (
+	`entry` mediumint (8),
+	`item` mediumint (8),
+	`ChanceOrQuestChance` float ,
+	`lootmode` smallint (5),
+	`groupid` tinyint (3),
+	`mincountOrRef` mediumint (9),
+	`maxcount` tinyint (3)
+); 
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','43102','77','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','47241','100','1','0','1','2');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50302','22','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50303','22','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50304','9','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50305','9','1','2','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50306','9','1','2','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50308','9','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50309','13','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50310','12','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50311','15','1','2','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50312','13','1','2','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50313','12','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202337','50314','12','1','1','1','1');
