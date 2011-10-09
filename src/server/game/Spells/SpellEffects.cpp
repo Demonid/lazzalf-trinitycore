dd52342ac3d7b672c37580ceac18f2ad64519443
M3i <u.mercogliano@gmail.com>
2011-10-09 23:30:20 +0200

    rereadd debug code for Steady Shot

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -778,7 +778,7 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
                             damage += int32(m_caster->ToPlayer()->GetAmmoDPS()*item->GetTemplate()->Delay*0.001f);
 							damage += int32((m_caster->ToPlayer()->GetTotalAttackPowerValue(RANGED_ATTACK)*0.1f) + 10);
 							sLog->outError("%s's RANGED_ATTACK is %f!", m_caster->ToPlayer()->GetName(), m_caster->ToPlayer()->GetTotalAttackPowerValue(RANGED_ATTACK) );
-							
+							sLog->outBoss("%s's RANGED_ATTACK is %f!", m_caster->ToPlayer()->GetName(), m_caster->ToPlayer()->GetTotalAttackPowerValue(RANGED_ATTACK) );							
                         }
                     }
                     // Glyph of Steady Shot
