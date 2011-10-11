e3803d8c6364ae3270ba3fa9ddfefcf00faafaf4
Lazzalf <Lazzalf@localhost>
2011-10-11 10:38:34 +0200

    Fixes in Dalaran Sewers arena to make use of Dynamic LoS. Thanks to kerhong & kapoeira

---
+++ sql/custom/27405_loot_GO_CAPTAIN_CHEST_4.sql
@@ -0,0 +1,20 @@
+/*
+SQLyog Community v9.20 
+MySQL - 5.1.49-1ubuntu8.1 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','43102','100','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','47241','100','1','0','1','2');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50302','21','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50303','22','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50304','8','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50306','8','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50308','9','1','2','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50309','13','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50311','15','1','2','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50312','13','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50313','12','1','1','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202336','50314','12','1','2','1','1');
