cf134d9fa7feb11f577954c69d8c8a956ec76222
Lazzalf <Lazzalf@localhost>
2011-10-22 11:25:24 +0200

    * Fix the correct exit from the dungeon finder queue. By devilcoredev. * HackFix to malygos

--- sql/custom/27725_malygos.sql
+++ sql/custom/27725_malygos.sql
@@ -89,9 +89,10 @@ UPDATE `creature_template` SET `ScriptName`='boss_malygos' WHERE `entry`=28859;
 UPDATE `creature_template` SET `ScriptName`='npc_power_spark' WHERE `entry`=30084;
 UPDATE `creature_template` SET `ScriptName`='npc_portal_eoe' WHERE `entry`=30118;
 UPDATE `creature_template` SET `ScriptName`='npc_hover_disk' WHERE `entry` IN (30234,30248);
-UPDATE `creature_template` SET `ScriptName`='npc_arcane_overload' WHERE `entry`=(30282,30245);
+UPDATE `creature_template` SET `ScriptName`='npc_arcane_overload' WHERE `entry`= 30282;
 UPDATE `creature_template` SET `ScriptName`='npc_wyrmrest_skytalon' WHERE `entry`=30161;
 UPDATE `creature_template` SET `ScriptName`='npc_alexstrasza_eoe' WHERE `entry`=32295;
+UPDATE `creature_template` SET `ScriptName`='' WHERE `entry`= 30245;
 
 DELETE FROM `spell_script_names` WHERE `spell_id`=56105;
 INSERT INTO `spell_script_names` VALUES (56105,'spell_malygos_vortex_dummy');
